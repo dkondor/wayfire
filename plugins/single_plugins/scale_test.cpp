dd20a9a9708877a4e448db56d92cbc09ccc9cafc
Kondor Dániel <kondor.dani@gmail.com>
2023-09-27 07:46:59 +0200

    Adapt to changes in Wayfire

--- plugins/single_plugins/scale_test.cpp
+++ plugins/single_plugins/scale_test.cpp
@@ -1,16 +1,24 @@
-#include <wayfire/plugin.hpp>
+#include <wayfire/core.hpp>
+#include <wayfire/util.hpp>
+#include <wayfire/signal-definitions.hpp>
 #include <wayfire/output.hpp>
+#include <wayfire/plugin.hpp>
+#include <wayfire/per-output-plugin.hpp>
 #include <wayfire/view.hpp>
+#include <wayfire/toplevel-view.hpp>
+#include <wayfire/plugins/common/shared-core-data.hpp>
 #include <wayfire/plugins/scale-signal.hpp>
+#include <wayfire/plugins/ipc/ipc-method-repository.hpp>
+#include <nlohmann/json.hpp>
 
-class wayfire_scale_test : public wf::plugin_interface_t
+class wayfire_scale_test : public wf::per_output_plugin_instance_t
 {
     wf::option_wrapper_t<std::string> app_id_filter{"scale_test/app_id"};
     wf::option_wrapper_t<bool> case_sensitive{"scale_test/case_sensitive"};
     wf::option_wrapper_t<bool> all_workspaces{"scale_test/all_workspaces"};
     bool active = false;
 
-    bool should_show_view(wayfire_view view) const
+    bool should_show_view(wayfire_toplevel_view view) const
     {
         const std::string& app_id_str = app_id_filter;
         if (app_id_str.empty())
@@ -36,57 +44,51 @@ class wayfire_scale_test : public wf::plugin_interface_t
   public:
     void init() override
     {
-        grab_interface->name = "scale_test";
-        grab_interface->capabilities = 0;
-
-        output->connect_signal("scale-filter", &view_filter);
-        output->connect_signal("scale-end", &scale_end);
-
-        output->add_activator(
-            wf::option_wrapper_t<wf::activatorbinding_t>{"scale_test/activate"},
-            &activate);
+        output->connect(&view_filter);
+        output->connect(&scale_end);
+        
+        output->add_key(wf::option_wrapper_t<wf::keybinding_t>{"scale_test/activate"}, &activate);
     }
 
     void fini() override
     {
         output->rem_binding(&activate);
-        output->disconnect_signal(&view_filter);
-        output->disconnect_signal(&scale_end);
+        view_filter.disconnect();
+        scale_end.disconnect();
     }
 
-    wf::activator_callback activate = [=] (auto)
+    wf::key_callback activate = [=] (auto)
     {
         active = true;
         if (!output->is_plugin_active("scale"))
         {
-            wf::activator_data_t data;
-            data.source = wf::activator_source_t::PLUGIN;
-            return output->call_plugin(all_workspaces ? "scale/toggle_all" : "scale/toggle", data);
+            nlohmann::json data;
+			data["output_id"] = output->get_id();
+			wf::shared_data::ref_ptr_t<wf::ipc::method_repository_t> repo;
+			repo->call_method(all_workspaces ? "scale/toggle_all" : "scale/toggle", data);
         } else
         {
-            output->emit_signal("scale-update", nullptr);
+			scale_update_signal signal;
+            output->emit(&signal);
         }
         return true;
     };
 
-    wf::signal_connection_t view_filter{[this] (wf::signal_data_t *data)
+    wf::signal::connection_t<scale_filter_signal> view_filter = [this] (scale_filter_signal* signal)
         {
             if (active)
             {
-                auto signal = static_cast<scale_filter_signal*>(data);
-                scale_filter_views(signal, [this] (wayfire_view v)
+                scale_filter_views(signal, [this] (wayfire_toplevel_view v)
                 {
                     return !should_show_view(v);
                 });
             }
-        }
-    };
+        };
 
-    wf::signal_connection_t scale_end{[this] (wf::signal_data_t *data)
+    wf::signal::connection_t<scale_end_signal> scale_end = [this] (scale_end_signal* data)
         {
             active = false;
-        }
-    };
+        };
 };
 
-DECLARE_WAYFIRE_PLUGIN(wayfire_scale_test);
+DECLARE_WAYFIRE_PLUGIN(wf::per_output_plugin_t<wayfire_scale_test>)

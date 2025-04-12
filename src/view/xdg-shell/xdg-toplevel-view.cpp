4cc79d2ab1bbfdc51d0af534d8a5fe15cff278f3
Kondor Dániel <kondor.dani@gmail.com>
2025-04-12 18:07:46 +0200

    xdg-shell, xwayland: do not focus newly mapped toplevels if the corresponding option is not set

--- src/view/xdg-shell/xdg-toplevel-view.cpp
+++ src/view/xdg-shell/xdg-toplevel-view.cpp
@@ -310,7 +310,19 @@ void wf::xdg_toplevel_view_t::map()
     adjust_view_output_on_map(this);
 
     xdg_toplevel_view_base_t::map();
-    wf::get_core().default_wm->focus_request(self());
+
+    /* We only focus a newly mapped view if the corresponding option is
+     * set or if there is no currently active view. */
+    auto active_view = wf::get_core().seat->get_active_view();
+    if (active_view && (active_view->role == wf::VIEW_ROLE_DESKTOP_ENVIRONMENT))
+    {
+        active_view = nullptr;
+    }
+
+    if (wf::get_core().default_wm->focus_on_map || (active_view == nullptr))
+    {
+        wf::get_core().default_wm->focus_request(self());
+    }
 
     /* Might trigger repositioning */
     set_toplevel_parent(this->parent);
